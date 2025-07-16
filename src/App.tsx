c207419e764e303732c3c6128ea8118eb831f33e
VANSH <vanshhingrajiya7@gmail.com>
2025-07-16 23:07:59 +0530

    Initialized project

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css'
+
+const App = () => {
+  return (
+    <div class="container">
+      <div class="login-box">
+        Login Page
+      </div>
+    </div>
+  )
+}
+
+export default App
